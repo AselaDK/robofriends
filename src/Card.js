14933ef6ddea793a23f0506f5536048654bbc9a0
AselaDK <aselax2@gmail.com>
2019-03-13 12:46:44 +0530

    mistakenly changed has changed

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
+
 import React from 'react';
 
-const Card = (props) => {
-	const {name, email, id} = props;
+const Card = (name, email, id) => {
+    //const {name, email, id} = props;
     return(
         <div className = 'tc bg-light-purple dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robots' src={`https://robohash.org/${id}?100x100` /*removed props.id */} />
             <div>
-                <h2>{name}</h2>	{/*<h2>{props.name}</h2>*/}
-                <p>{email}</p>	{/*<p>{props.email}</p>*/}
+                <h2>{name}</h2> {/*<h2>{props.name}</h2>*/}
+                <p>{email}</p>  {/*<p>{props.email}</p>*/}
             </div> 
         </div>
     );
