2372d33cb1834979e767373a19a012c027e5bb39
AselaDK <aselax2@gmail.com>
2019-03-24 07:21:43 +0530

    adding all robot friends using map and a CardList.js

---
+++ src/CardList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+	//const cardComponent = 
+	return(
+		<div>
+			{
+				robots.map((user, i) => {
+					return (
+						<Card 
+						key={i} 
+						id={robots[i].id} 
+						name={robots[i].name} 
+						email={robots[i].email} 
+						/>
+					);
+				//returning each by mapping
+				})
+			}
+			{//cardComponent
+			}
+			{// mapped card component returned
+			}
+		</div>
+	/*
+	copied from indez.js to get all the details as an array o give rendering to index.js back
+	*/ 
+	);
+}
+
+export default CardList;	//exporting card list to render by index.js
+// then we have to access the robot details by giving 'props'
