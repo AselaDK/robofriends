9daea41b203e64e1af52f15ca7c733951d5a4ec6
AselaDK <aselax2@gmail.com>
2019-03-26 23:21:54 +0530

    unsaved are made saved 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+//import Card from './Card';	//removed beacuse never used
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
 import { robots } from './robots';	//put robots -> {robots} coz robots arn't default
@@ -9,12 +10,11 @@ import { robots } from './robots';	//put robots -> {robots} coz robots arn't def
 
 ReactDOM.render(
 	<div>
-		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-		<Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
-		<Card id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
-		<Card id={robots[5].id} name={robots[5].name} email={robots[5].email}/>
+		<CardList robots = {robots} />
+	{/*
+	CardList for render
+	refering robots array from robots.js to load details
+	*/}
 	</div>
 
 	, document.getElementById('root'));
