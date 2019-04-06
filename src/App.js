1cbe73a3045264d90cd9995ec1a3d19e1b8f96c0
AselaDK <aselax2@gmail.com>
2019-04-06 12:57:56 +0530

    commented what i did & added some css with App.css

--- src/App.js
+++ src/App.js
@@ -7,29 +7,35 @@ import { robots } from './robots';	//put robots -> {robots} coz robots arn't def
 
 import SearchBox from './SearchBox';
 
+import './App.css';
+
 class App extends Component{    //changed the function to a class, same App component
     constructor() {
         super()         //if using this.
-        this.state = {
+        this.state = {      //state of this class to render
             robots: robots, //robots array
-            searchfield: ''
+            searchfield: '' //searchfield
         }
     }
 
-    onSearchChange = (event) => {
+    // function to set state
+    onSearchChange = (event) => { 
         this.setState( {searchfield: event.target.value} )
+        // event.target.value gives the search term
     }
 
     render(){
+        //filter robots through searchbox
         const filteredrobots = this.state.robots.filter(robot =>{
             return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+            //filter out robots robot.name(searchbox)
         })
         //console.log(filterrobots);
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange} />
-                <CardList robots = {filteredrobots} /> {/* pasted from index.js*/}
+                <h1 className = 'f1'>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange} />  {/* set search change event */}
+                <CardList robots = {filteredrobots} /> {/* pasted from index.js*/}  {/* show filtered robots */}
             </div>
     );
     }
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // creating search box like html
-const SearchBox = ({ searchChange }) => {
+const SearchBox = ({ searchChange }) => {       //gave searchChange state(function)
     return (
         <div className='pa2'>
             <input 
                 className='pa3 ba b--green bg-lightest-blue'
                 type='search' //seaech is an input type
                 placeholder='search robots' 
-                onChange = {searchChange}
+                onChange = {searchChange}   //set on change function state
             />
         </div>
     );
