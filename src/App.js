ae1ce33ef7f5db10d7fcf5cddceb13a4aa9a8ba5
AselaDK <aselax2@gmail.com>
2019-04-06 22:03:56 +0530

    Data users got through an API. disabled robots.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React, {Component} from 'react'; //this is to remove React. in app export
 import CardList from './CardList';
-import { robots } from './robots';	//put robots -> {robots} coz robots arn't default
+//import { robots } from './robots';	//put robots -> {robots} coz robots arn't default
 //we can import like {robots, cats, dogs, ...}
 
 import SearchBox from './SearchBox';
@@ -13,7 +13,7 @@ class App extends Component{    //changed the function to a class, same App comp
     constructor() {
         super()         //if using this.
         this.state = {      //state of this class to render
-            robots: robots, //robots array
+            robots: [], //robots array
             searchfield: '' //searchfield
         }
     }
@@ -24,6 +24,15 @@ class App extends Component{    //changed the function to a class, same App comp
         // event.target.value gives the search term
     }
 
+    // pure function check the component did mount
+    componentDidMount(){
+        //fetch http request through link
+        //receive response 
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response =>  response.json())         //gettin response
+            .then(users => this.setState({robots: users}));    //setState according o the response
+    }
+
     render(){
         //filter robots through searchbox
         const filteredrobots = this.state.robots.filter(robot =>{
@@ -31,13 +40,19 @@ class App extends Component{    //changed the function to a class, same App comp
             //filter out robots robot.name(searchbox)
         })
         //console.log(filterrobots);
-        return (
-            <div className='tc'>
-                <h1 className = 'f1'>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange} />  {/* set search change event */}
-                <CardList robots = {filteredrobots} /> {/* pasted from index.js*/}  {/* show filtered robots */}
-            </div>
-    );
+
+        //managing tons of users loading... when slow or.... with loading.
+        if(this.state.robots.length === 0){
+            return <h1>Loading...</h1>
+        }else{
+            return (
+                <div className='tc'>
+                    <h1 className = 'f1'>RoboFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChange} />  {/* set search change event */}
+                    <CardList robots = {filteredrobots} /> {/* pasted from index.js*/}  {/* show filtered robots */}
+                </div>
+        );
+        }
     }
     
 }
